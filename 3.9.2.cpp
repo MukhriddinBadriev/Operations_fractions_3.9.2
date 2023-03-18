7775b6d11c8fa6acb1dba9a8e3992ec0e0dfbb8b
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2023-03-19 01:13:52 +0300

    Fraction + std::gcd

--- 3.9.2.cpp
+++ 3.9.2.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <numeric>
 using namespace std;
 
 class Fraction
@@ -6,96 +7,35 @@ class Fraction
 private:
 	int numerator_;
 	int denominator_;
-
 public:
 	Fraction(int numerator, int denominator)
 	{
-
-		numerator_ = numerator;
+		numerator_ = numerator / std::gcd(numerator, denominator);
 		if (denominator == 0) {
 			cout << "ОШИБКА: ПОПЫТКА ДЕЛИТЬ НА НОЛЬ" << endl;
 			exit(0);
 		}
-		else denominator_ = denominator;
+		else denominator_ = denominator / std::gcd(numerator, denominator);
 	}
 
 	Fraction operator+(Fraction other) {
-		if ((numerator_ * other.denominator_ + denominator_ * other.numerator_) <= (denominator_ * other.denominator_)) {
-			for (int i = (numerator_ * other.denominator_ + denominator_ * other.numerator_); i >=2 ; i--) {
-				if ((numerator_ * other.denominator_ + denominator_ * other.numerator_) % i == 0 && (denominator_ * other.denominator_) % i == 0) {
-					return Fraction((numerator_ * other.denominator_ + denominator_ * other.numerator_) / i, (denominator_ * other.denominator_) / i);
-				}
-			}
-		}
-		else {
-			for (int i = (denominator_ * other.denominator_); i >=2 ; i--) {
-				if ((numerator_ * other.denominator_ + denominator_ * other.numerator_) % i == 0 && (denominator_ * other.denominator_) % i == 0) {
-					return Fraction((numerator_ * other.denominator_ + denominator_ * other.numerator_) / i, (denominator_ * other.denominator_) / i);
-				}
-			}
-		}
-		return Fraction((numerator_ * other.denominator_ + denominator_ * other.numerator_) , (denominator_ * other.denominator_) );
+		return Fraction((numerator_ * other.denominator_ + denominator_ * other.numerator_), (denominator_ * other.denominator_));
 	}
 	Fraction operator-(Fraction other) {
-		if ((numerator_ * other.denominator_ - denominator_ * other.numerator_) <= (denominator_ * other.denominator_)) {
-			for (int i = (numerator_ * other.denominator_ - denominator_ * other.numerator_); i>=2; i--) {
-				if ((numerator_ * other.denominator_ - denominator_ * other.numerator_) % i == 0 && (denominator_ * other.denominator_) % i == 0) {
-					return Fraction((numerator_ * other.denominator_ - denominator_ * other.numerator_)/i,
-						(denominator_ * other.denominator_)/i);
-				}
-			}
-		}
-		else  {
-			for (int i = (denominator_ * other.denominator_); i >=2 ; i--) {
-				if ((numerator_ * other.denominator_ + denominator_ * other.numerator_) % i == 0 && (denominator_ * other.denominator_) % i == 0) {
-					return Fraction((numerator_ * other.denominator_ - denominator_ * other.numerator_)/i,
-						(denominator_ * other.denominator_)/i);
-				}
-			}
-		}
 		return Fraction((numerator_ * other.denominator_ - denominator_ * other.numerator_),
 			(denominator_ * other.denominator_));
 	}
 	Fraction operator*(Fraction other) {
-		if ((numerator_ * other.numerator_) <= (denominator_ * other.denominator_)) {
-			for (int i = (numerator_ * other.numerator_); i >=2 ; i--) {
-				if ((numerator_ * other.numerator_) % i == 0 && (denominator_ * other.denominator_) % i == 0) {
-					return Fraction((numerator_ * other.numerator_)/i, (denominator_ * other.denominator_)/i);
-				}
-			}
-		}
-		else {
-			for (int i = (denominator_ * other.denominator_); i >=2 ; i--) {
-				if ((numerator_ *other.numerator_) % i == 0 && (denominator_ * other.denominator_) % i == 0) {
-					return Fraction((numerator_ * other.numerator_)/i, (denominator_ * other.denominator_) / i);
-				}
-			}
-		}
 		return Fraction((numerator_ * other.numerator_), (denominator_ * other.denominator_));
 	}
 	Fraction operator/(Fraction other) {
-		if ((numerator_ * other.denominator_) <= (other.numerator_ * denominator_)) {
-			for (int i = (numerator_ * other.denominator_); i >=2 ; i--) {
-				if ((numerator_ * other.denominator_) % i == 0 && (other.numerator_ * denominator_) % i == 0) {
-					return Fraction((numerator_ * other.denominator_)/i, (other.numerator_ * denominator_)/i);
-				}
-			}
-		}
-		else {
-			for (int i = (other.numerator_ * denominator_); i >=2 ; i--) {
-				if ((numerator_ * other.denominator_ + denominator_ * other.numerator_) % i == 0 && (denominator_ * other.denominator_) % i == 0) {
-					return Fraction((numerator_ * other.denominator_)/i, (other.numerator_ * denominator_)/i);
-				}
-			}
-		}
 		return Fraction((numerator_ * other.denominator_), (other.numerator_ * denominator_));
 	}
 	Fraction& operator++() { numerator_ += denominator_; denominator_; return *this; }
 	Fraction operator++(int)
 	{
 		Fraction temp(numerator_, denominator_);
-		numerator_ = numerator_ + denominator_;
-		//denominator_ = denominator_;
+		numerator_ = numerator_ + denominator_;		
 		return temp;
 	}
 	Fraction& operator--() { numerator_ -= denominator_; denominator_; return *this; }
@@ -111,7 +51,7 @@ public:
 
 ostream& operator<<(ostream& os, const Fraction& fr)
 {
-	os << fr.numerator_ <<"/"<<fr.denominator_;
+	os << fr.numerator_ << "/" << fr.denominator_;
 	return os;
 }
 
@@ -133,14 +73,14 @@ int main()
 
 	Fraction f1(numerator_1, denominator_1);
 	Fraction f2(numerator_2, denominator_2);
-	
+
 	cout << numerator_1 << "/" << denominator_1 << " + " << numerator_2 << "/" << denominator_2 << " = " << (f1 + f2) << endl;
 	cout << numerator_1 << "/" << denominator_1 << " - " << numerator_2 << "/" << denominator_2 << " = " << (f1 - f2) << endl;
 	cout << numerator_1 << "/" << denominator_1 << " * " << numerator_2 << "/" << denominator_2 << " = " << (f1 * f2) << endl;
 	cout << numerator_1 << "/" << denominator_1 << " / " << numerator_2 << "/" << denominator_2 << " = " << (f1 / f2) << endl;
 	cout << "++" << numerator_1 << "/" << denominator_1 << " * " << numerator_2 << "/" << denominator_2 << " = " << (++f1 * f2) << endl;
-	cout << "Значение дроби 1 = " << f1<<endl;
-	cout  << numerator_1 << "/" << denominator_1 << " -- * " << numerator_2 << "/" << denominator_2 << " = " << ((f1--) * f2) << endl;
-	cout << "Значение дроби 1 = " << f1<<endl;
+	cout << "Значение дроби 1 = " << f1 << endl;
+	cout << numerator_1 << "/" << denominator_1 << " -- * " << numerator_2 << "/" << denominator_2 << " = " << ((f1--) * f2) << endl;
+	cout << "Значение дроби 1 = " << f1 << endl;
 	return 0;
 }
